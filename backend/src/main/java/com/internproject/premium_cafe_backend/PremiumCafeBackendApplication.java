1894a6c63668536545cfef5bf68ee6b9785ebb78
raghuram1845 <raghuramroyya2022@gmail.com>
2026-07-18 19:45:29 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/internproject/premium_cafe_backend/PremiumCafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.internproject.premium_cafe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumCafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremiumCafeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/internproject/premium_cafe_backend/PremiumCafeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internproject.premium_cafe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiumCafeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
